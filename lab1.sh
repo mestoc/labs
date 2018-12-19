8d061ecd4b66c2b0e10c91e162722e8395154fce
mestoc <pandadragneel@gmail.com>
2018-12-20 00:31:56 +0700

    final

--- lab1.sh
+++ lab1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+wget http://factorized.net/crusoe.txt 
 sed -e "s/ /\n/g" crusoe.txt | grep -i friday | wc -l
-
+sed -e "s/Friday/Saturday/gi" crusoe.txt > temp.txt && rm crusoe.txt && mv temp.txt crusoe.txt
+sed -e "s/ /\n/g" crusoe.txt | grep -i friday | wc -l
